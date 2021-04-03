6cffa615fe787980c4d541509cd6e5bf156cf2ea
Toyin96 <onagoruwam@gmail.com>
2021-04-03 03:35:18 +0100

    created and activated virtualenv, Django blog, and updating settings.py

---
+++ toyin_blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+# We're creating a Post class that inherits from Model
+class Post(models.Model):
+    """"""
