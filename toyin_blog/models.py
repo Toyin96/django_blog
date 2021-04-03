03ccb44728e8062179ae3c1bf79465a25e1dc0f3
Toyin96 <onagoruwam@gmail.com>
2021-04-03 06:50:09 +0100

    created post model

--- toyin_blog/models.py
+++ toyin_blog/models.py
@@ -5,4 +5,19 @@ from django.utils import timezone
 
 # We're creating a Post class that inherits from Model
 class Post(models.Model):
-    """"""
+    """Creating a Django model, post."""
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        """publishes an article/post"""
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        """renders the class in string format"""
+        return self.title
+
